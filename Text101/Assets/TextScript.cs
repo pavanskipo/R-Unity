6d6ef7f6b12534b97ac0b19f4b3d043e4aec8bf0
pavanskipo <pavanskipo@gmail.com>
2019-04-12 20:12:17 +0530

    Text based on Unity

---
+++ Text101/Assets/TextScript.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TextScript : MonoBehaviour {
+
+    [SerializeField] Text textField;
+    [SerializeField] State startingState;
+
+    State state;
+
+	// Use this for initialization
+	void Start () {
+        state = startingState;
+        textField.text = state.getStateStory();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        manageState();
+	}
+
+    private void manageState() {
+        var nextStates = state.getNextStates();
+        if (Input.GetKeyDown(KeyCode.Alpha1)) {
+            state = nextStates[0];
+        } else if (Input.GetKeyDown(KeyCode.Alpha2)) {
+            state = nextStates[1];
+        } else if (Input.GetKeyDown(KeyCode.Alpha3)) {
+            state = nextStates[2];
+        }
+
+        textField.text = state.getStateStory();
+
+    }
+}
